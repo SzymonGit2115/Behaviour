966a08caef87416126011789b2b712ba7da4c1b1
SzymonGit2115 <dsw45519@student.dsw.edu.pl>
2023-06-01 20:44:41 +0200

    State_Machine_

---
+++ Assets/Scripts/MovementSM.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementSM :  StateMachine
+
+{
+
+    [HideInInspector]
+    public Idle idleState;
+    [HideInInspector]
+    public Moving movingState;
+
+    public Rigidbody2D rigidbody;
+    public SpriteRenderer spriteRenderer;
+    
+    public float speed = 4f;
+
+  
+
+
+    private void Awake()
+    {
+        idleState = new Idle(this);
+        movingState = new Moving(this);
+       
+    }
+
+    protected override BaseState GetInitialState()
+    {
+        return idleState;
+    }
+}
